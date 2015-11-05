5b997c33d6863b1d00a6362d108cca4fb10f056e
Yoonseok <passionvirus@gmail.com>
2015-11-05 11:32:59 +0900

    changed the pkg name

--- turtlebot_arm_4dof_demo/setup.py
+++ turtlebot_arm_4dof_demo/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['turtlebot_arm_navigation'],
+    packages=['turtlebot_arm_4dof_demo'],
     package_dir={'': 'src'},
 )
 
